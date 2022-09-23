e0cb70c3062c66cee691857c7e3183fb09462a6c
Jaakko Pero <jaakko.pero@lianatech.com>
2022-09-23 12:11:04 +0300

    initial coding standards

--- lianaautomation-woocommerce.php
+++ lianaautomation-woocommerce.php
@@ -1,24 +1,20 @@
 <?php
 /**
- * Plugin Name:       LianaAutomation - WooCommerce
- * Plugin URI:        https://www.lianatech.com/solutions/websites
+ * Plugin Name:       LianaAutomation WooCommerce
  * Description:       LianaAutomation for WooCommerce.
  * Version:           1.0.39
  * Requires at least: 5.2
  * Requires PHP:      7.4
- * Author:            Liana Technologies
+ * Author:            Liana Technologies Oy
  * Author URI:        https://www.lianatech.com
  * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0-standalone.html
  * Text Domain:       lianaautomation
  * Domain Path:       /languages
- * 
+ *
  * PHP Version 7.4
- * 
- * @category Components
- * @package  WordPress
- * @author   Liana Technologies <websites@lianatech.com>
- * @author   Jaakko Pero <jaakko.pero@lianatech.com>
+ *
+ * @package  LianaAutomation
  * @license  https://www.gnu.org/licenses/gpl-3.0-standalone.html GPL-3.0-or-later
  * @link     https://www.lianatech.com
  */
@@ -26,27 +22,29 @@
 /**
  * Include cookie handler code
  */
-require_once dirname(__FILE__) . '/includes/lianaautomation-cookie.php';
+require_once dirname( __FILE__ ) . '/includes/lianaautomation-cookie.php';
 
 /**
  * Include WooCommerce Order Status handler code
  */
-require_once dirname(__FILE__)
-    . '/includes/lianaautomation-woocommerce-orderstatus.php';
+require_once dirname( __FILE__ )
+	. '/includes/lianaautomation-woocommerce-orderstatus.php';
 
 /**
  * Include WooCommerce Login handler code
  */
-require_once dirname(__FILE__)
-    . '/includes/lianaautomation-woocommerce-login.php';
+require_once dirname( __FILE__ )
+	. '/includes/lianaautomation-woocommerce-login.php';
 
 /**
  * Include WooCommerce Customer handler code
  */
-require_once dirname(__FILE__)
-    . '/includes/lianaautomation-woocommerce-customer.php';
+require_once dirname( __FILE__ )
+	. '/includes/lianaautomation-woocommerce-customer.php';
 
 /**
- * Include admin panel code
+ * Conditionally include admin panel code
  */
-require_once dirname(__FILE__) . '/admin/lianaautomation-admin.php';
+if ( is_admin() ) {
+	require_once dirname( __FILE__ ) . '/admin/lianaautomation-admin.php';
+}
